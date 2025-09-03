c995e2f1d2a9a1e208aa559b28b13548dc016812
jerrygitdemo <jerrygitdemo@gmail.com>
2025-09-03 19:34:24 +0800

    完成單一縣市json格式回傳

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, Response
 from datetime import datetime
-from pm25 import get_data_from_mysql, write_data_to_mysql, get_avg_pm25_from_mysql
+from pm25 import (
+    get_data_from_mysql,
+    write_data_to_mysql,
+    get_avg_pm25_from_mysql,
+    get_pm25_by_county,
+)
 import json
 
 books = {
@@ -23,6 +28,39 @@ books = {
 
 
 app = Flask(__name__)
+
+
+@app.route("/county-pm25/<county>")
+def get_county_pm25(county):
+    result = get_pm25_by_county(county)
+
+    if len(result) == 0:
+        return Response(
+            json.dumps(
+                {"result": "取得資料失敗", "message": f"無此({county})資料"},
+                ensure_ascii=False,
+            )
+        )
+
+    # 加入try except
+    site = [r[0] for r in result]
+    pm25 = [float(r[1]) for r in result]
+    datetime = result[0][2].strftime("%Y-%m-%d %H:%M:%S")
+    print(datetime)
+
+    return Response(
+        json.dumps(
+            {
+                "county": county,
+                "count": len(site),
+                "datetime": datetime,
+                "site": site,
+                "pm25": pm25,
+            },
+            ensure_ascii=False,
+        ),
+        mimetype="application/json",
+    )
 
 
 @app.route("/avg-pm25")
