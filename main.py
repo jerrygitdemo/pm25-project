7ae5acbafe572e72c33a2fc9c26e5f476dc2ecab
jerrygitdemo <jerrygitdemo@gmail.com>
2025-09-05 19:02:27 +0800

    完成發布前的準備

--- main.py
+++ main.py
@@ -83,6 +83,7 @@ def update_db():
     return json.dumps(result, ensure_ascii=False)
 
 
+@app.route("/")
 @app.route("/pm25")
 def get_pm25():
     values, countys = get_data_from_mysql()
@@ -117,10 +118,10 @@ def now_time():
     return time
 
 
-@app.route("/")
 def index():
     time = now_time()
     return render_template("index.html", x=time, name="jerry")
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=False)
